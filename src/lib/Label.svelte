28b2785ccec6398d068d741b5e579e329f7f6da1
biblio <biblio@bib102bd_08pub.unal.loc>
2023-09-18 13:04:12 -0500

    componente label creado

---
+++ src/lib/Label.svelte
@@ -0,0 +1,76 @@
+<script>
+    const stringToColour = (str) => {
+        let hash = 0;
+        str.split('').forEach(char => {
+            hash = char.charCodeAt(0) + ((hash << 5) - hash)
+        })
+        let colour = '#'
+        for (let i = 0; i < 3; i++) {
+            const value = (hash >> (i * 8)) & 0xff
+            colour += value.toString(16).padStart(2, '0')
+        }
+        return colour
+    }
+
+    function invertColor(hex, bw) {
+        if (hex.indexOf('#') === 0) {
+            hex = hex.slice(1);
+        }
+        // convert 3-digit hex to 6-digits.
+        if (hex.length === 3) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        if (hex.length !== 6) {
+            throw new Error('Invalid HEX color.');
+        }
+        var r = parseInt(hex.slice(0, 2), 16),
+            g = parseInt(hex.slice(2, 4), 16),
+            b = parseInt(hex.slice(4, 6), 16);
+        if (bw) {
+            // https://stackoverflow.com/a/3943023/112731
+            return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+                ? '#000000'
+                : '#FFFFFF';
+        }
+        // invert color components
+        r = (255 - r).toString(16);
+        g = (255 - g).toString(16);
+        b = (255 - b).toString(16);
+        // pad each with zeros and return
+        return "#" + padZero(r) + padZero(g) + padZero(b);
+    }
+
+    let text = 'CEEDO'
+    let backgroundColor = stringToColour(text)
+    let textColor = invertColor(backgroundColor, true)
+</script>
+
+
+
+<div style="--bg-color: {backgroundColor}; --text-color: {textColor}">
+    {text} 
+    <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6.15173 0.848328L0.848433 6.15163" stroke="{textColor}"/>
+        <path d="M6.15173 6.15167L0.848433 0.848371" stroke="{textColor}"/>
+    </svg>
+</div>
+
+<style>
+    div {
+        background-color: var(--bg-color);
+        color: var(--text-color);
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        font-family: 'Inter';
+        font-weight: lighter;
+        padding-block: 3px;
+        padding-inline: 4px;
+        width: max-content;
+        border-radius: 5px;
+    }
+
+    svg{
+        margin-left: 5px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Label from "../lib/Label.svelte";
+</script>
+
+<Label></Label>
